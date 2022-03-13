250e321433fa7563bfc90fbb8fb7e9e8708033c8
Piyush Jha <jhapiyush06@gmail.com>
2022-03-13 10:21:24 +0530

    Added signin & signout routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,12 +19,61 @@ exports.signup = (req, res) => {
                     error: err
                 });
             }
-            // res.json({
-            //     user: success
-            // });
             res.json({
                 message: 'Signup success! Please signin.'
             });
         });
+    });
+};
+
+exports.signin = (req, res) => {
+    const { email, password } = req.body;
+    // check if user exist
+    User.findOne({ email }).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User with that email does not exist. Please signup.'
+            });
+        }
+        // authenticate
+        if (!user.authenticate(password)) {
+            return res.status(400).json({
+                error: 'Email and password do not match.'
+            });
+        }
+        // generate a token and send to client
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+        res.cookie('token', token, { expiresIn: '1d' });
+        const { _id, email } = user;
+        return res.json({
+            token,
+            user: { _id, email }
+        });
+    });
+};
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ["HS256"],
+});
+
+exports.authMiddleware = (req, res, next) => {
+    const authUserId = req.user._id;
+    User.findById({ _id: authUserId }).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User not found'
+            });
+        }
+        req.profile = user;
+        next();
+    });
+};
+
+exports.signout = (req, res) => {
+    res.clearCookie('token');
+    res.json({
+        message: 'Signout success'
     });
 };
