c1e06f70984da6f51159b5dadff87ba106354198
Piyush Jha <jhapiyush06@gmail.com>
2022-03-21 02:26:02 +0530

    Added latest movies routes

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const date = require('../helpers/date');
 
 exports.movies = (req, res) => {
     axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_key}&language=en-US&page=1`)
@@ -10,7 +11,15 @@ exports.movies = (req, res) => {
 };
 
 exports.latestmovies = (req, res) => {
-    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_key}&language=en-US&page=1`)
+    console.log(date.getTodayDate());
+    axios.get(`https://api.themoviedb.org/3/discover/movie`,{
+        params: {
+            'api_key': process.env.API_key,
+            'language': 'en-US',
+            'include_adult': false,
+            'sort_by': 'release_date.desc',
+            'release_date.lte':date.getTodayDate()
+    }})
     .then( response => 
         res
         .status(200)
